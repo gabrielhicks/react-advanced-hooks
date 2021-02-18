7d9090924a8a5c4fe0c82c99f92ea3d69c821cd4
gabrielhicks <gabrielhicks@mail.com>
2021-02-18 14:32:02 -0500

    Redux convention

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,29 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action),
-})
+const countReducer = (state, action) => {
+  const {type, step} = action
+  switch (type) {
+    case 'INCREMENT': {
+      return {
+        ...state,
+        count: state.count + step,
+      }
+    }
+    default: {
+      throw new Error('This type is not supported')
+    }
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
 
   const {count} = state
 
-  const increment = () => setState({count: count + step})
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
